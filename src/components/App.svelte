107b52edbf46cd23d70914ed710f9a68f18f87a4
Arvind S <arvind0598@gmail.com>
2019-12-10 20:34:16 +0530

    changed layout

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -39,8 +39,10 @@ a {
   text-decoration: underline;
 }
 
-.column--main {
-  text-align: center;
+.hero-body {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
 }
 
 * {
@@ -51,20 +53,14 @@ a {
 
 <section class="hero is-fullheight is-primary is-bold">
   <div class="hero-body">
+    <div class="container is-fluid is-centered has-text-centered">
+      <h1 class="title is-2"> Wikipedia Viewer </h1>
+      <p class="subtitle is-4"> Search for an article, or get a random one <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank">here</a>.</p>
+      <SearchBox bind:searchTerm={searchTerm} on:submit={fetchData}/>
+    </div>
     <div class="container is-fluid">
-      <div class="columns is-vcentered">
-        <div class="column--main column is-4">
-          <h1 class="title is-2"> Wikipedia Viewer </h1>
-          <p class="subtitle is-4"> Search for an article, or get a random one <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank">here</a>.</p>
-          <SearchBox bind:searchTerm={searchTerm} on:submit={fetchData}/>
-        </div>
-        <div class="column">
-          <div class="container">
-            <div class="columns is-multiline">
-              <ResultsBox searchData={searchData}/>
-            </div>
-          </div>
-        </div>
+      <div class="columns is-multiline">
+        <ResultsBox searchData={searchData}/>
       </div>
     </div>
   </div>
--- src/components/ResultsBox.svelte
+++ src/components/ResultsBox.svelte
@@ -14,7 +14,7 @@ afterUpdate(() => {
 {#if searchData.status === 'FETCHED'}
   {#if searchData.results.length > 0}
     {#each results as data(data.id)}
-      <div class="column is-half">
+      <div class="column is-3">
         <SingleResult title={data.title} snippet={data.snippet}/>
       </div>
     {/each}
